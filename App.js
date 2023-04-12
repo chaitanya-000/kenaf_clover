120072727a5f598f54e4991d6af4b6f097204f82
Chaitanya Joshi <Chaitanya99j@gmail.com>
2023-04-12 17:57:09 +0530

    Checkpoint - Adds navigation blueprint

--- App.js
+++ App.js
@@ -15,9 +15,44 @@ import CompanyDetails from "./src/screens/CompanyDetails";
 import ProfileUpdate from "./src/screens/ProfileUpdate";
 import Analytics from "./src/screens/Analytics";
 import Loyalty from "./src/screens/Loyalty";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
-  return <Loyalty />;
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Dashboard" component={Dashboard} />
+      <Tab.Screen name="Payment" component={Payment} />
+      <Tab.Screen name="Loyalty" component={Loyalty} />
+      <Tab.Screen name="Analytics" component={Analytics} />
+    </Tab.Navigator>
+  );
 }
 
-const styles = StyleSheet.create({});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="HomeTabs" component={HomeTabs} />
+        <Stack.Screen name="ProfileUpdate" component={ProfileUpdate} />
+        <Stack.Screen name="CompanyDetails" component={CompanyDetails} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen
+          name="PaymentDetails_Refund"
+          component={PaymentDetails_Refund}
+        />
+        <Stack.Screen
+          name="PaymentDetails_Receipt"
+          component={PaymentDetails_Receipt}
+        />
+        <Stack.Screen name="PaymentLogs" component={PaymentLogs} />
+        <Stack.Screen name="QuickSale" component={QuickSale} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
