4bb7a457f6f72295a48d2bb973e73847d174f14d
Chaitanya Joshi <Chaitanya99j@gmail.com>
2023-04-13 16:28:52 +0530

    Drawer_Naviagtion: checkpoint (continue working from here)

--- src/DrawerNavigation.js
+++ src/DrawerNavigation.js
@@ -1,21 +1,112 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import TabsNavigation from "./TabsNavigation";
 import ProfileUpdate from "./screens/ProfileUpdate";
 import CompanyDetails from "./screens/CompanyDetails";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+import Settings from "./screens/Settings";
+import Icon from "react-native-vector-icons/Ionicons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-const DrawerNavigation = () => {
+const DrawerNavigation = (props) => {
   const Drawer = createDrawerNavigator();
+
+  const handleLogout = () => {
+    // add your logout functionality here
+  };
+
+  const CustomDrawerContent = (props) => {
+    return (
+      <DrawerContentScrollView {...props} style={styles.drawerContent}>
+        <DrawerItemList {...props} />
+        <TouchableOpacity onPress={handleLogout}>
+          <View style={styles.logoutButton}>
+            <MaterialCommunityIcons
+              name="logout"
+              size={24}
+              color="white"
+              style={{ marginRight: 10 }}
+            />
+            <Text style={styles.logoutText}>Logout</Text>
+          </View>
+        </TouchableOpacity>
+      </DrawerContentScrollView>
+    );
+  };
+
   return (
-    <Drawer.Navigator screenOptions={{ headerShown: false }}>
-      <Drawer.Screen name="Home" component={TabsNavigation} />
-      <Drawer.Screen name="ProfileUpdate" component={ProfileUpdate} />
-      <Drawer.Screen name="CompanyDetails" component={CompanyDetails} />
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        headerShown: false,
+      }}
+      drawerStyle={styles.drawer}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={TabsNavigation}
+        options={{
+          drawerIcon: ({ focused, size }) => (
+            <Icon
+              name={focused ? "home" : "home-outline"}
+              size={size}
+              color={focused ? "#007AFF" : "white"}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Links"
+        component={CompanyDetails}
+        options={{
+          drawerIcon: ({ focused, size }) => (
+            <MaterialCommunityIcons
+              name={focused ? "link-variant" : "link-variant-outline"}
+              size={size}
+              color={focused ? "#007AFF" : "white"}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          drawerIcon: ({ focused, size }) => (
+            <Icon
+              name={focused ? "settings" : "settings-outline"}
+              size={size}
+              color={focused ? "#007AFF" : "white"}
+            />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 };
 
-export default DrawerNavigation;
+const styles = StyleSheet.create({
+  drawer: {
+    backgroundColor: "rgba(18, 31, 39, 1)",
+  },
+  drawerContent: {
+    backgroundColor: "rgba(18, 31, 39, 1)",
+  },
+  logoutButton: {
+    marginTop: 10,
+    marginLeft: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  logoutText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
 
-const styles = StyleSheet.create({});
+export default DrawerNavigation;
