7a30da4306d048580e6167baa292f8338772c33e
Chaitanya Joshi <Chaitanya99j@gmail.com>
2023-04-20 20:14:14 +0530

    API : PaymentDetails_Receipt - integrates TransactionSuccess API for fetching the Transaction details

--- src/screens/PaymentDetails_Receipt.js
+++ src/screens/PaymentDetails_Receipt.js
@@ -1,12 +1,5 @@
-import {
-  Image,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
 import {
   BackButton,
   ButtonText,
@@ -21,29 +14,113 @@ import {
   WhiteRoundedContainer,
 } from "../../styledComponents";
 import { Ionicons } from "@expo/vector-icons";
-import { responsiveFontSize } from "../../helperFunction";
-const PaymentDetails_Receipt = () => {
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { BASE_URL } from "../../helperFunction";
+
+const PaymentDetails_Receipt = ({ navigation }) => {
+  const [paymentDetails, setPaymentDetails] = useState(null);
+
+  const date = new Date(paymentDetails?.created_at);
+  const formattedDate = date.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  const formattedTime = date.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+
+  useEffect(() => {
+    const getTillId = async () => {
+      try {
+        const value = await AsyncStorage.getItem("tID");
+        if (value !== null) {
+          axios
+            .post(`${BASE_URL}/TransactionSuccess`, {
+              tId: JSON.parse(value),
+            })
+            .then((response) => {
+              console.log(response.data.data);
+              setPaymentDetails(response.data.data);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    getTillId();
+  }, []);
+
   return (
     <PageContainer>
       <Header>
-        <BackButton>
+        <BackButton onPress={() => navigation.navigate("DrawerNavigation")}>
           <Ionicons name="md-arrow-back" size={25} color="white" />
         </BackButton>
       </Header>
       <WhiteRoundedContainer>
         <PageContent>
           <View style={styles.amountAndPaymentStatusContainer}>
-            <Text style={styles.amount}>$240:00</Text>
-            <Text style={styles.paymentStatus}>Payment Not Closed</Text>
+            <Text style={styles.amount}>€ {paymentDetails?.Amount / 100}</Text>
+            <Text style={styles.paymentStatus}>
+              {paymentDetails?.description}
+            </Text>
           </View>
           <View style={styles.dateAndTimeContainer}>
-            <Text style={styles.dateAndTime}>July 5 2021</Text>
+            <Text style={styles.dateAndTime}>{formattedDate}</Text>
             <View style={styles.separatingDot}></View>
-            <Text style={styles.dateAndTime}>15:30 PM</Text>
+            <Text style={styles.dateAndTime}>{formattedTime}</Text>
           </View>
-          <View style={styles.detailsReceipt}></View>
+          <View style={styles.detailsReceipt}>
+            <View style={styles.infoContainer}>
+              <Text>Amount</Text>
+              <Text>€ {paymentDetails?.Amount / 100}</Text>
+            </View>
+            <View style={styles.infoContainer}>
+              <Text>Card Type</Text>
+              <Text>{paymentDetails?.cardType}</Text>
+            </View>
+            <View style={styles.infoContainer}>
+              <Text>Card </Text>
+              <Text>{paymentDetails?.cardDetails}</Text>
+            </View>
+            <View style={styles.infoContainer}>
+              <Text>Till ID</Text>
+              <Text>{paymentDetails?.tId}</Text>
+            </View>
+            <View style={styles.infoContainer}>
+              <Text>Trs ID</Text>
+              <Text>{paymentDetails?.transactionId}</Text>
+            </View>
+            <View
+              style={{
+                width: "100%",
+                borderWidth: 0.5,
+                borderColor: "#DEE8EF",
+              }}
+            ></View>
+            <View style={[styles.infoContainer, { marginTop: 10 }]}>
+              <Text style={{ fontSize: 20 }}>Amount</Text>
+              <Text
+                style={{
+                  fontSize: 20,
+                  fontWeight: "700",
+                  color: "rgba(38, 174, 96, 1)",
+                }}
+              >
+                € {paymentDetails?.Amount / 100}
+              </Text>
+            </View>
+          </View>
           <SolidGreenButton width={"100%"} height={"11%"}>
-            <ButtonText>Print Receipt</ButtonText>
+            <ButtonText>Print to QR code</ButtonText>
           </SolidGreenButton>
           <LineDivider>
             <HalfPart></HalfPart>
@@ -51,7 +128,7 @@ const PaymentDetails_Receipt = () => {
             <HalfPart></HalfPart>
           </LineDivider>
           <TouchableOpacity>
-            <InformativeFooterText>Print to QR code</InformativeFooterText>
+            <InformativeFooterText>Print Receipt</InformativeFooterText>
           </TouchableOpacity>
         </PageContent>
       </WhiteRoundedContainer>
@@ -99,15 +176,25 @@ const styles = StyleSheet.create({
   },
   detailsReceipt: {
     width: "100%",
-    height: "45%",
+    height: "50%",
     backgroundColor: "#F6F8FA",
     borderRadius: 15,
     borderWidth: 1,
     borderColor: "#DEE8EF",
     marginBottom: "10%",
+    padding: "3%",
+    alignItems: "center",
   },
   buttonText: {
     color: "white",
     fontWeight: "700",
+  },
+  infoContainer: {
+    width: "90%",
+    height: "15%",
+    // borderWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
 });
